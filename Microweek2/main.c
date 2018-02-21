e8f1da1acd3a00eb597fbfee8a31653bae150ba4
LAPTOP-PO4KSU0T\Gijs <gcmfesse@avans.nl>
2018-02-21 20:40:02 +0100

    second commit

--- Microweek2/main.c
+++ Microweek2/main.c
@@ -8,7 +8,41 @@
 #include <avr/io.h>
 #include <util/delay.h>
 #include <avr/interrupt.h>
+void opdracht4();
 
+const unsigned char Numbers [16] =
+{
+	// dPgfe dcba
+	0b00111111, // 0
+	0b00000110, // 1
+	0b01011011, // 2
+	0b01001111, // 3
+	0b01100110, // 4
+	0b01101101, // 5
+	0b01111101, // 6
+	0b00000111, // 7
+	0b01111111, // 8
+	0b01101111, // 9
+	0b01110111, // 10
+	0b01111100, // 11
+	0b00111001, // 12
+	0b01011110, // 13
+	0b01111001, // 14
+	0b01110001, // 15
+};
+
+const unsigned char patern [8] =
+{
+	// dPgfe dcba
+	0b00100000, // 0
+	0b00000001, // 1
+	0b00000010, // 2
+	0b01000000, // 3
+	0b00010000, // 4
+	0b00001000, // 5
+	0b00000100, // 6
+	0b01000000, // 7
+};
 
 /******************************************************************/
 void wait( int ms )
@@ -27,6 +61,8 @@ Version :    	DMK, Initial code
 		_delay_ms( 1 );		// library function (max 30 ms at 8MHz)
 	}
 }
+int x = 0;
+
 
 /******************************************************************/
 ISR( INT0_vect )
@@ -38,7 +74,16 @@ notes:			Set PORTD.5
 Version :    	DMK, Initial code
 *******************************************************************/
 {
-    PORTD |= (1<<5);		
+	//if(x<=8){
+		//PORTD |= (1<<x);
+		//x++;
+	//}	
+	if(x==15){
+		x=0;
+	}
+	PORTC = Numbers[x];
+	x++;
+		wait(1000);
 }
 
 /******************************************************************/
@@ -51,7 +96,17 @@ notes:			Clear PORTD.5
 Version :    	DMK, Initial code
 *******************************************************************/
 {
-    PORTD &= ~(1<<5);		
+	//if(x>=4){
+		//PORTD &= ~(1<<x);
+		//x--;
+	//}
+	PORTC = Numbers[x];
+	x--;
+	if(x==0){
+		x=15;
+	}
+	wait(1000);
+				
 }
 
 /******************************************************************/
@@ -66,21 +121,33 @@ Version :    	DMK, Initial code
 {
 	// Init I/O
 	DDRD = 0xF0;			// PORTD(7:4) output, PORTD(3:0) input	
+	DDRC = 0xFF;
 
 	// Init Interrupt hardware
 	EICRA |= 0x0B;			// INT1 falling edge, INT0 rising edge
 	EIMSK |= 0x03;			// Enable INT1 & INT0
 	
+	//opdracht4();
+	
 	// Enable global interrupt system
 	//SREG = 0x80;			// Of direct via SREG of via wrapper
-	sei();				
+	sei();			
 
 	while (1)
 	{
-		PORTD ^= (1<<7);	// Toggle PORTD.7
-		wait( 500 );								
+		//PORTD ^= (1<<7);	// Toggle PORTD.7
+		//wait( 500 );								
 	}
 
 	return 1;
+}
+
+void opdracht4(){
+	int index = 0;
+	while(index<sizeof(patern)){
+		PORTC = patern[index];
+		wait(1000);
+		index++;
+	}
 }
 
